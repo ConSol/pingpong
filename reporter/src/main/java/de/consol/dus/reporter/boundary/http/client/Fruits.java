1451ed317843e818b38be2000a7c38274c0c99e4
Marco Bungart <marco.bungart@googlemail.com>
2022-09-01 15:30:34 +0200

    added logging

--- reporter/src/main/java/de/consol/dus/reporter/boundary/http/client/Fruits.java
+++ reporter/src/main/java/de/consol/dus/reporter/boundary/http/client/Fruits.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Log4j2
 @Service
 public class Fruits {
   private static final String DEFAULT_FRUIT_NAME = "banana";
@@ -25,16 +27,24 @@ public class Fruits {
   }
 
   public String getRandomFruitName() {
-    final ResponseEntity<Fruit[]> response = restTemplate.getForEntity(baseUri, Fruit[].class);
-    if (response.getStatusCode() == HttpStatus.OK) {
-      List<String> fruitNames = Arrays.stream(response.getBody())
-          .map(Fruit::getName)
-          .collect(Collectors.toList());
-      if (!fruitNames.isEmpty()) {
-        int index = random.nextInt(fruitNames.size());
-        return fruitNames.get(index);
+    try {
+      final ResponseEntity<Fruit[]> response = restTemplate.getForEntity(baseUri, Fruit[].class);
+      if (response.getStatusCode() == HttpStatus.OK) {
+        List<String> fruitNames = Arrays.stream(response.getBody())
+            .map(Fruit::getName)
+            .collect(Collectors.toList());
+        if (!fruitNames.isEmpty()) {
+          int index = random.nextInt(fruitNames.size());
+          return fruitNames.get(index);
+        }
       }
+    } catch (Exception e) {
+      log.error("Error", e);
     }
+    log.warn(
+        "Unable to get fruit from {}, using default fruit name {}",
+        baseUri.toString(),
+        DEFAULT_FRUIT_NAME);
     return DEFAULT_FRUIT_NAME;
   }
 }
